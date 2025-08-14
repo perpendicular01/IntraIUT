d6ab63223fc51d994511063bf9a472ba8d9de3fc
saif <kh.s.k.rohan@gmail.com>
2025-08-14 07:44:10 +0600

    server initial setup

---
+++ intraIutServer/src/main/java/com/iut/intraiutserver/IntraIutServerApplication.java
@@ -0,0 +1,13 @@
+package com.iut.intraiutserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntraIutServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntraIutServerApplication.class, args);
+    }
+
+}
---
+++ intraIutServer/src/main/java/com/iut/intraiutserver/entities/User.java
@@ -0,0 +1,43 @@
+package com.iut.intraiutserver.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "user_name", nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Column(length = 500) 
+    private String about;
+}
