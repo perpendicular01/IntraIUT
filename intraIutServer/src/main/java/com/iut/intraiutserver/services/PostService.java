424f563f8c900affb68b561b3ecad0e9a7b3c49b
miraj <mirajmahmudmahee@gmail.com>
2025-08-15 00:20:36 +0600

    postController create delete apis work

--- intraIutServer/src/main/java/com/iut/intraiutserver/services/PostService.java
+++ intraIutServer/src/main/java/com/iut/intraiutserver/services/PostService.java
@@ -1,4 +1,17 @@
 package com.iut.intraiutserver.services;
 
+<<<<<<< Updated upstream
 public class PostService {
 }
+=======
+
+import com.iut.intraiutserver.payloads.PostDto;
+
+import java.util.List;
+
+public interface PostService {
+    PostDto createPost(PostDto postDto, String username, Integer categoryId);
+    void deletePost(Integer postId);
+
+}
+>>>>>>> Stashed changes
--- intraIutServer/src/main/java/com/iut/intraiutserver/services/impl/PostServiceImpl.java
+++ intraIutServer/src/main/java/com/iut/intraiutserver/services/impl/PostServiceImpl.java
@@ -1,4 +1,75 @@
 package com.iut.intraiutserver.services.impl;
 
+<<<<<<< Updated upstream
 public class PostServiceImpl {
 }
+=======
+
+import com.iut.intraiutserver.entities.Category;
+import com.iut.intraiutserver.entities.Post;
+import com.iut.intraiutserver.entities.PostStatus;
+import com.iut.intraiutserver.entities.User;
+import com.iut.intraiutserver.exceptions.ResourceNotFoundException;
+import com.iut.intraiutserver.payloads.PostDto;
+import com.iut.intraiutserver.repositories.CategoryRepo;
+import com.iut.intraiutserver.repositories.PostRepo;
+import com.iut.intraiutserver.repositories.UserRepo;
+import com.iut.intraiutserver.services.PostService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    @Autowired
+    private PostRepo postRepo;
+    @Autowired
+    private ModelMapper modelMapper;
+    @Autowired
+    private UserRepo userRepo;
+    @Autowired
+    private CategoryRepo categoryRepo;
+
+
+
+    @Override
+    public PostDto createPost(PostDto postDto, String username, Integer categoryId) {
+        User user = this.userRepo.findByEmail(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User", "email", username));
+        Category category = this.categoryRepo.findById(categoryId)
+                .orElseThrow(() -> new ResourceNotFoundException("Category", "category id", categoryId));
+        Post post = this.modelMapper.map(postDto, Post.class);
+        post.setImageName("default.png");
+        post.setAddedDate(new Date());
+        post.setUser(user);
+        post.setCategory(category);
+        post.setStatus(PostStatus.PENDING);
+        Post newPost = this.postRepo.save(post);
+        return this.modelMapper.map(newPost, PostDto.class);
+    }
+
+    @Override
+    public void deletePost(Integer postId) {
+        Post post = this.postRepo.findById(postId)
+                .orElseThrow(() -> new ResourceNotFoundException("Post", "post id", postId));
+
+        this.postRepo.delete(post);
+    }
+
+
+
+
+
+
+}
+>>>>>>> Stashed changes
