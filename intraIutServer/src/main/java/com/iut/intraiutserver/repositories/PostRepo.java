d58c43db184d59626805a9f00d1be02b71cf337c
mueed <mueedibnesami@iut-dhaka.edu>
2025-08-15 00:06:47 +0600

    post create services

--- intraIutServer/src/main/java/com/iut/intraiutserver/repositories/PostRepo.java
+++ intraIutServer/src/main/java/com/iut/intraiutserver/repositories/PostRepo.java
@@ -1,4 +1,13 @@
 package com.iut.intraiutserver.repositories;
 
-public class PostRepo {
+import com.iut.intraiutserver.entities.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PostRepo extends JpaRepository<Post, Integer> {
+
+
 }
